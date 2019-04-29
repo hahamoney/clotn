c7555165d6644c7842add5d1480c20113d6432dc
Ara112 <wx_a1590c154b2f4ba6883de0942a6dde1f@git.code.tencent.com>
2019-04-29 17:15:01 +0800

    add page merchant/merchantsetail

--- pages/merchant/merchantdetail.js
+++ pages/merchant/merchantdetail.js
@@ -8,59 +8,42 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onClickHome() {
+    wx.navigateBack({
+      delta: 1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClickCall: function () {
+    wx.makePhoneCall({
+      phoneNumber: '8008208820',
+      success: function () {
+        console.log('调起拨打界面')
+      },
+      fail: function () {
+        console.log('拨打失败')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  merchantapply: function () {
+    wx.navigateTo({
+      url: '/pages/merchant/apply',
+    })
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '产品详情',
+      path: '/page/user?id=123'
+    }
   }
+
 })
