45c9a5d477b1f67c121618ce7ed6177deb69ade9
XxElza <873639465@qq.com>
2019-05-02 01:41:20 +0800

    申请页提交 获取openid

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ App({
   data:{
     mapkey:'IHTBZ-7V5CR-4WZWT-WNDFX-7B2V5-ZEFYY',
     api:'http://cloth.com/api/',
-    image:'http://cloth.com/'
+    image:'http://cloth.com/',
+    appid:'wx716a760549d1ba15',
+    appSecret:'c02165caa149933c3687cf4d33e09e68'
     },
   onLaunch: function () {
-  },
+
+  },showMsg(title){
+    wx.showToast({
+      title: title,
+      icon:'none'
+    })
+  }
 })
--- pages/merchant/apply.js
+++ pages/merchant/apply.js
@@ -17,7 +17,9 @@ Page({
     facility:'',
     logo:'/image/merchant.png',
     wechat:'/image/merchant.png',
-    image_list:[]
+    image_list:[],
+    merchant_detail_image: [],
+    merchant_detail_list:[],
   },
 
 
@@ -112,29 +114,143 @@ Page({
     });
   },
   upload_logo(){
+    this.upload(1); 
+  },
+  upload_wechat() {
+    this.upload(2); 
+  },
+  upload_detail() {
+    var _this = this;
+    if (_this.data.merchant_detail_list.length>6){
+      return false;
+    }
+          this.upload(3); 
+  },
+
+  upload(type){
     var _this=this;
-    wx.chooseImage({
+    var image,list;
+    if(type == 1){
+      image = _this.data.logo;
+    }
+
+    if(type == 2){
+      image = _this.data.wechat;
+    }
+
+    list = _this.data.image_list;
+
+    if(type == 3){
+      image = _this.data.merchant_detail_image;
+      list = _this.data.merchant_detail_list;
+    }
+
+      wx.chooseImage({
       success(res) {
         wx.uploadFile({
           url: app.data.api + 'merchant_image',
           filePath: res.tempFilePaths[0],
           name: 'file',
           formData: {
-            type: '1'
+            type: type
           },
           success(res) {
-            var data = JSON.parse(res.data);
-              var  list=_this.data.image_list
-              list.push(data.id);
+           var data= JSON.parse(res.data);
+            list.push(data.id);
+            if(type == 3){
+              image.push(app.data.image+data.path);
               _this.setData({
-                logo:app.data.image+data.path,
-                image_list:list
+                merchant_detail_image:image,
+                merchant_detail_list:list
               })
+              return false;  
+            }
+
+            _this.setData({
+              image_list:list
+            })
+
+            if(type == 1){
+              _this.setData({
+                logo:app.data.image+data.path
+              })
+              return false;
+            }
+
+            if(type == 2){
+              _this.setData({
+                wechat:app.data.image+data.path
+              })
+              return false;
+            }
+
           }
         })
       }
     })
   },
+  formSubmit(e){
+     var _this=this;
+     var data=e.detail.value;;
+     console.log(data);
+    if (data.merchant_name==null||data.merchant_name.replace(/\s*/g, "")==''){
+      app.showMsg('名称不能为空');
+      return;
+     }
+
+    if (data.merchant_city == null) {
+      app.showMsg('地址不能为空');
+      return;
+    }
+    if (data.merchant_keyword == null || data.merchant_keyword.replace(/\s*/g, "") == '') {
+      app.showMsg('关键字不能为空');
+      return;
+    }
+    if (data.announcement == null || data.announcement.replace(/\s*/g, "") == '') {
+      app.showMsg('商家介绍不能为空');
+      return;
+    }
+    if (data.facility.length==0) {
+      app.showMsg('商家设施不能为空');
+      return;
+    }
+
+    wx.request({
+      url: app.data.api + 'merchant_enter',
+      method:'post',
+      dataType:'Json',
+      data:{
+        merchant_name: data.merchant_name,
+        merchant_type: _this.data.merchant_classify[data.merchant_type]['id'],
+        merchant_keyword: data.merchant_keyword,
+        merchant_city: data.merchant_city,
+        facility: data.facility,
+        announcement: data.announcement,
+        image_list: _this.data.image_list,
+        merchant_detail_list:_this.data.merchant_detail_list,
+        user_id :1,
+        longitude: _this.data.longitude,
+        latitude:_this.data.latitude,
+      },success(res){
+        console.log(res);
+      }
+    })
+  }, bindGetUserInfo(e){
+     console.log(e);
+    wx.login({
+      success(res) {
+            console.log(res);
+        var url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + app.data.appid+'&secret=' + app.data.appSecret+'&js_code='+res.code+'&grant_type=authorization_code';
+            wx.request({
+              url: url,
+              success(res){
+                console.log(res);
+              }
+            })
+      }
+    })
+  }
+
 
 
 })
