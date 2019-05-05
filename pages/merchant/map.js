cdcd45e3735615831c99355abaa441085d1dc2fd
AJ <im.knock@yahoo.com.hk>
2019-05-05 23:21:42 +0800

    add fail function to reselect location auth

--- pages/merchant/map.js
+++ pages/merchant/map.js
@@ -30,6 +30,56 @@ Page({
       },
       fail: function (err) {
         console.log(err)
+        wx.getSetting({
+          success: function (res) {
+            var statu = res.authSetting;
+            if (!statu['scope.userLocation']) {
+              wx.showModal({
+                title: '是否授权当前位置',
+                content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                success: function (tip) {
+                  if (tip.confirm) {
+                    wx.openSetting({
+                      success: function (data) {
+                        if (data.authSetting["scope.userLocation"] === true) {
+                          wx.showToast({
+                            title: '授权成功',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                          //授权成功之后，再调用chooseLocation选择地方
+                          wx.chooseLocation({
+                            success: function (res) {
+                              console.log(res);
+                              //选择地点之后返回到原来页面
+                              wx.setStorageSync('latitude', res.latitude);
+                              wx.setStorageSync('address', res.address);
+                              wx.setStorageSync('longitude', res.longitude);
+                              wx.navigateBack();
+                            },
+                          })
+                        } else {
+                          wx.showToast({
+                            title: '授权失败',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '调用授权窗口失败',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        })
       }
     });
   }
