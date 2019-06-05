e5bd4a2084272b0bd3bfb665b09370ea773ea347
liulei <873639465@qq.com>
2019-06-05 14:09:04 +0800

    擦边球

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 App({
   data:{
     mapkey:'IHTBZ-7V5CR-4WZWT-WNDFX-7B2V5-ZEFYY',
-    api:'http://cloth.com/api/',
+   // api:'http://cloth.com/api/',
+    api:'https://cloth.fumos.club/api/',
     image:'http://cloth.com/',
     appid:'wx716a760549d1ba15',
     appSecret:'c02165caa149933c3687cf4d33e09e68'
